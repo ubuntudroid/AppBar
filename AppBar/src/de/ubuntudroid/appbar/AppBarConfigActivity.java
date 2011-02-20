65b6d861b4d6b0308803f220053d3f8ab1b926be
ubuntudroid <ubuntudroid@googlemail.com>
2011-02-20 16:44:32 +0100

    initial commit

---
+++ AppBar/src/de/ubuntudroid/appbar/AppBarConfigActivity.java
@@ -0,0 +1,13 @@
+package de.ubuntudroid.appbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AppBarConfigActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
