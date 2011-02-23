68502390951efce817d3d25a87e02f5865a16eba
ubuntudroid <ubuntudroid@googlemail.com>
2011-02-23 02:24:04 +0100

    - notifications are now displayed properly - fixed fetching recent tasks

--- AppBar/src/de/ubuntudroid/appbar/AppBarNotification.java
+++ AppBar/src/de/ubuntudroid/appbar/AppBarNotification.java
@@ -4,13 +4,21 @@ import android.widget.RemoteViews;
 
 public class AppBarNotification extends RemoteViews {
 
-	public AppBarNotification(String packageName, int layoutId) {
+	private AppBarService service;
+	
+	public AppBarNotification(String packageName, int layoutId, AppBarService service) {
 		super(packageName, layoutId);
 		// TODO Auto-generated constructor stub
 	}
 	
-	public AppBarNotification(){
+	public AppBarNotification(AppBarService service){
 		super("de.ubuntudroid.appbar", R.layout.app_bar_notification);
+	}
+	
+	@Override
+	public boolean onLoadClass(Class clazz) {
+		// TODO Auto-generated method stub
+		return super.onLoadClass(clazz);
 	}
 
 }
--- AppBar/src/de/ubuntudroid/appbar/AppBarService.java
+++ AppBar/src/de/ubuntudroid/appbar/AppBarService.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.TreeMap;
 
 import android.app.ActivityManager;
+import android.app.ActivityManager.RecentTaskInfo;
 import android.app.ActivityManager.RunningAppProcessInfo;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -44,63 +48,80 @@ public class AppBarService extends Service {
 	public void onStart(Intent intent, int startId) {
 		super.onStart(intent, startId);
 		
-		runningApps = am.getRunningAppProcesses();
-		//TODO: check for the correct order by comparing with the list retrieved last time
-		
-		List<RunningAppProcessInfo> latestApps = getLatestApps(runningApps);
-		List<Bitmap> appIcons = getLatestAppIcons(latestApps);
+		List<RecentTaskInfo> recentTasks = am.getRecentTasks(50, 0);
+		List<RecentTaskInfo> fiveRecentTasks = getFiveRecentTasks(recentTasks);
+		List<Bitmap> appIcons = getRecentTaskIcons(fiveRecentTasks);
+		List<PendingIntent> appPendingIntents = getRecentTaskPendingIntents(fiveRecentTasks);
 		
 		AppBarNotification appBarNotification = new AppBarNotification(getPackageName(), R.layout.app_bar_notification);
 		prepareButtonBitmaps(appBarNotification, appIcons);
-		
+		prepareButtonClickListeners(appBarNotification, appPendingIntents);
 //		appBarNotification.setOnClickPendingIntent(R.id.app1, pendingIntent);
 		
 		Notification notification = new Notification();
 		notification.contentView = appBarNotification;
+		
+		Intent notificationIntent = new Intent();
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+		notification.contentIntent = contentIntent;
+		notification.icon = R.drawable.icon;
 		
 		nm.notify(APP_BAR_NOTIFICATION, notification);
 	}
 
 	private void prepareButtonBitmaps(AppBarNotification appBarNotification, List<Bitmap> appIcons) {
 		appBarNotification.setImageViewBitmap(R.id.app1, appIcons.get(0));
-		appBarNotification.setImageViewBitmap(R.id.app1, appIcons.get(1));
-		appBarNotification.setImageViewBitmap(R.id.app1, appIcons.get(2));
-		appBarNotification.setImageViewBitmap(R.id.app1, appIcons.get(3));
-		appBarNotification.setImageViewBitmap(R.id.app1, appIcons.get(4));
+		appBarNotification.setImageViewBitmap(R.id.app2, appIcons.get(1));
+		appBarNotification.setImageViewBitmap(R.id.app3, appIcons.get(2));
+		appBarNotification.setImageViewBitmap(R.id.app4, appIcons.get(3));
+		appBarNotification.setImageViewBitmap(R.id.app5, appIcons.get(4));
+	}
+	
+	private void prepareButtonClickListeners(AppBarNotification appBarNotification, List<PendingIntent> appPendingIntents) {
+		appBarNotification.setOnClickPendingIntent(R.id.app1, appPendingIntents.get(0));
+		appBarNotification.setOnClickPendingIntent(R.id.app2, appPendingIntents.get(1));
+		appBarNotification.setOnClickPendingIntent(R.id.app3, appPendingIntents.get(2));
+		appBarNotification.setOnClickPendingIntent(R.id.app4, appPendingIntents.get(3));
+		appBarNotification.setOnClickPendingIntent(R.id.app5, appPendingIntents.get(4));
 	}
 
-	private List<RunningAppProcessInfo> getLatestApps(List<RunningAppProcessInfo> runningApps) {
-		
-		//The sorting is based on the fact, that the smaller the lru of a process, the more recently it was used
-		
-		TreeMap<Integer, RunningAppProcessInfo> runningAppsAndLrus = new TreeMap<Integer, ActivityManager.RunningAppProcessInfo>();
-		
-		for (RunningAppProcessInfo pi:runningApps){
-			runningAppsAndLrus.put(pi.lru, pi);
+	private List<RecentTaskInfo> getFiveRecentTasks(List<RecentTaskInfo> recentTasks) {
+		List<RecentTaskInfo> fiveRecentTasks = new ArrayList<RecentTaskInfo>();
+		int i = 0;
+		for (RecentTaskInfo rti: recentTasks){
+			if (i > 4){
+				return fiveRecentTasks;
+			}
+			
+			Set<String> categories = rti.baseIntent.getCategories();
+			if (categories != null && categories.contains(Intent.CATEGORY_LAUNCHER)){
+				fiveRecentTasks.add(rti);
+				i++;
+			}
 		}
-		
-		Collection<RunningAppProcessInfo> collection = runningAppsAndLrus.values();
-		Iterator<RunningAppProcessInfo> iter = collection.iterator();
-				
-		List<RunningAppProcessInfo> firstFiveAppsList = new ArrayList<ActivityManager.RunningAppProcessInfo>();
-		for (int i = 0; i < 5; i++){
-			firstFiveAppsList.add(iter.next());
-		}
-		
-		return firstFiveAppsList;
+		return fiveRecentTasks;
 	}
 
-	private List<Bitmap> getLatestAppIcons(List<RunningAppProcessInfo> latestApps) {
+	private List<Bitmap> getRecentTaskIcons(List<RecentTaskInfo> recentTasks) {
 		List<Bitmap> icons = new ArrayList<Bitmap>();
-		for (RunningAppProcessInfo pi: latestApps){
+		for (RecentTaskInfo rti : recentTasks){
 			try {
-				icons.add(((BitmapDrawable) getPackageManager().getApplicationIcon(pi.pkgList[0])).getBitmap());
+				icons.add(((BitmapDrawable) getPackageManager().getApplicationIcon(rti.baseIntent.getComponent().getPackageName())).getBitmap());
 			} catch (NameNotFoundException e) {
-				// TODO: cycle through all packages in pkgList until we find the correct one
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		return icons;
 	}
-	
+
+	private List<PendingIntent> getRecentTaskPendingIntents(List<RecentTaskInfo> recentTasks) {
+		List<PendingIntent> appPendingIntents = new ArrayList<PendingIntent>();
+		int i = 0;
+		for (RecentTaskInfo rti : recentTasks){
+			appPendingIntents.add(PendingIntent.getActivity(getApplicationContext(), 0, rti.baseIntent, 0));
+			i++;
+		}
+		return appPendingIntents;
+	}
 }
