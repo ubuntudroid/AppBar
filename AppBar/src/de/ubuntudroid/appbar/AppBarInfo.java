e14829c26eadfeb1f8e2bd05678bc4390b942b36
Sven Bendel <ubuntudroid@googlemail.com>
2012-01-20 15:34:09 +0100

    added GPL licence

--- AppBar/src/de/ubuntudroid/appbar/AppBarInfo.java
+++ AppBar/src/de/ubuntudroid/appbar/AppBarInfo.java
@@ -1,3 +1,23 @@
+/**
+ * AppBar - a simple application switcher for Android
+ * Copyright (C) 2012  Sven Bendel (ubuntudroid@googlemail.com)
+ *
+ * This file is part of AppBar.
+ *
+ * AppBar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * AppBar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AppBar. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.ubuntudroid.appbar;
 
 import android.app.Activity;
